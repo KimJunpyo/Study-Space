22be18258c2849d2c52eeb510ae8ccb4ede69f73
jkim <jkim@blast-team.com>
2025-05-30 23:57:17 +0900

    feat: scope chain visibility feature

--- scope-chain/index.js
+++ scope-chain/index.js
@@ -9,46 +9,75 @@ runButton.addEventListener("click", () => {
     sourceType: "module",
   });
 
-  console.log(ast);
   const path = [];
 
   const scopeChainList = getScopeChain(ast, path);
-  console.log(scopeChainList);
+  const resultDiv = document.querySelector("#result");
+  resultDiv.innerHTML = renderScopeChain(scopeChainList);
 });
+
+function renderScopeChain(scopeChainList) {
+  return scopeChainList
+    .map((item) => {
+      return `<div>${item.name}'s Scope Chain: <br/>${item.scopeChain
+        .split("-")
+        .map(
+          (scope, index) =>
+            `<div style="margin-left: ${index > 1 ? (index - 1) * 15 : 0}px;">${
+              index !== 0 ? "└ " : ""
+            }${scope}</div>`
+        )
+        .join("")} type: ${item.type}</div>`;
+    })
+    .join("");
+}
 
 function getScopeChain(ast, currentPath) {
   const result = [];
   if (ast.type === "Program") {
-    currentPath.push("Global");
-    result.push({
-      scopeChain: currentPath.join(","),
-      name: "Global",
-      type: "global",
-    });
+    currentPath.push("global");
+    result.push(createScopeItem(currentPath, "Global", "global"));
   }
   if (ast.type === "VariableDeclaration") {
-    result.push({
-      scopeChain: currentPath.join("-"),
-      name: ast.declarations[0].id.name,
-      type: "variable",
-    });
+    const declarations = ast.declarations;
+    const name = getDeclarationName(declarations[0]);
+    const newPath = extendPath(currentPath, name);
+    if (isArrowFunction(declarations[0])) {
+      result.push(createScopeItem(newPath, name, "arrow-function"));
+      addScope(result, declarations[0].init.body, newPath);
+    } else {
+      result.push(createScopeItem(newPath, name, "variable"));
+    }
   }
   if (ast.type === "FunctionDeclaration") {
-    result.push({
-      scopeChain: currentPath.join(","),
-      name: ast.id.name,
-      type: "function",
-    });
-    const scopeChain = getScopeChain(ast.body, [...currentPath, ast.id.name]);
-    result.push(...scopeChain);
+    const newPath = extendPath(currentPath, ast.id.name);
+    result.push(createScopeItem(newPath, ast.id.name, "function"));
+    addScope(result, ast.body, newPath);
   }
 
   if (ast.body && ast.body.length > 0) {
     ast.body.forEach((body) => {
-      const scopeChain = getScopeChain(body, currentPath);
-      result.push(...scopeChain);
+      addScope(result, body, currentPath);
     });
   }
 
   return result;
 }
+
+const isArrowFunction = (declaration) =>
+  declaration.init && declaration.init.type === "ArrowFunctionExpression";
+
+const getDeclarationName = (declaration) => declaration.id.name;
+
+const extendPath = (currentPath, newPath) => [...currentPath, newPath];
+
+const addScope = (result, node, currentPath) => {
+  const scopeChain = getScopeChain(node, currentPath);
+  result.push(...scopeChain);
+};
+
+const createScopeItem = (currentPath, name, type) => ({
+  scopeChain: currentPath.join("-"),
+  name: name,
+  type: type,
+});
